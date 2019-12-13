ec20965f7c2c3d0fec968b8570c6c7c3c0ef3e0e
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2019-12-13 20:59:36 +0100

    Test bool subcast for list

--- tests/test_typecast/test_list.py
+++ tests/test_typecast/test_list.py
@@ -1,3 +1,8 @@
 def test_list(set_env, env):
     set_env({"VALID_LIST": "one item,another item,3rd item"})
     assert env.list("VALID_LIST") == ["one item", "another item", "3rd item"]
+
+
+def test_list_subcast_bool(set_env, env):
+    set_env({"BOOL_LIST": "1,false,true,FaLSE,0"})
+    assert env.list("BOOL_LIST", subcast=bool) == [True, False, True, False, False]
