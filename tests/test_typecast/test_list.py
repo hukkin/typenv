a9291f6d1d4a1b20074c75a22f01befa38387f87
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2019-12-15 18:29:17 +0100

    Implement dump and get_example

--- tests/test_typecast/test_list.py
+++ tests/test_typecast/test_list.py
@@ -3,6 +3,11 @@ def test_list(set_env, env):
     assert env.list("VALID_LIST") == ["one item", "another item", "3rd item"]
 
 
+def test_list_empty(set_env, env):
+    set_env({"EMPTY_LIST": ""})
+    assert env.list("EMPTY_LIST") == []
+
+
 def test_list_subcast_bool(set_env, env):
     set_env({"BOOL_LIST": "1,false,true,FaLSE,0"})
     assert env.list("BOOL_LIST", subcast=bool) == [True, False, True, False, False]
