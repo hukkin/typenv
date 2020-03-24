93f19d23b4f9eb13855c77e1a22f6bfeaec739af
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-03-24 02:26:58 +0100

    Typehint pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,17 @@
+from typing import Callable, Mapping
+
 import pytest
 
 from typenv import Env
 
 
 @pytest.fixture
-def env():
+def env() -> Env:
     return Env()
 
 
 @pytest.fixture
-def set_env(monkeypatch):
+def set_env(monkeypatch) -> Callable[[Mapping], None]:
     def _set_env(env_map):
         for k, v in env_map.items():
             monkeypatch.setenv(k, v)
