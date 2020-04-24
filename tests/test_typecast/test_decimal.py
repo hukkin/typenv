f105f92d52cedc0f44b198a67d422183377d64a4
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-04-24 16:48:27 +0200

    Test env.decimal() return type

--- tests/test_typecast/test_decimal.py
+++ tests/test_typecast/test_decimal.py
@@ -3,6 +3,9 @@ from decimal import Decimal as D
 
 def test_decimal(set_env, env):
     set_env({"INT": "2", "FLOAT": "2.0", "MISSING_0": ".21"})
+    assert isinstance(env.decimal("INT"), D)
     assert env.decimal("INT") == D(2)
+    assert isinstance(env.decimal("FLOAT"), D)
     assert env.decimal("FLOAT") == D("2.0")
+    assert isinstance(env.decimal("MISSING_0"), D)
     assert env.decimal("MISSING_0") == D(".21")
